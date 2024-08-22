0e15873e2c2e6bd3013aa7edfaa710263d8519f5
Khizarali07 <khizarali.cs@gmail.com>
2024-08-22 03:59:48 -0700

    one problem fix

--- src/mycomponents/ProductList.js
+++ src/mycomponents/ProductList.js
@@ -15,15 +15,15 @@ export default function ProductList() {
     <div id='container'>
     <div className='items' id='k1'>
         <div className='ione'>Samsung Galaxy S10<span className='span'>100000</span></div>
-        <div className='itwo'><span className='min' onClick={()=>{pricedown('k1')}} onMouseOut={foot}>-</span><span className='quan'>0</span><span className='max' onClick={()=>{priceup('k1')}} onMouseOut={foot}>+</span></div>
+        <div className='itwo'><span className='min' onClick={()=>{pricedown('k1'); foot();}} >-</span><span className='quan'>0</span><span className='max' onClick={()=>{priceup('k1'); foot();}} onMouseOut={foot}>+</span></div>
         <div className='ithree'>0</div>
-        <div className='ifour' onClick={()=>{remove('k1')}} onMouseOver={foot}>Remove</div>
+        <div className='ifour' onClick={()=>{remove('k1');}} >Remove</div>
     </div>
     <div className='items' id='k2'>
         <div className='ione'>Samsung Galaxy S11<span className='span'>150000</span></div>
-        <div className='itwo'><span className='min' onClick={()=>{pricedown('k2')}} onMouseOut={foot}>-</span><span className='quan'>0</span><span className='max' onClick={()=>{priceup('k2')}} onMouseOut={foot}>+</span></div>
+        <div className='itwo'><span className='min' onClick={()=>{pricedown('k2'); foot();}} >-</span><span className='quan'>0</span><span className='max' onClick={()=>{priceup('k2'); foot();}} onMouseOut={foot}>+</span></div>
         <div className='ithree'>0</div>
-        <div className='ifour' onClick={()=>{remove('k2')}} onMouseOver={foot}>Remove</div>
+        <div className='ifour' onClick={()=>{remove('k2');}} >Remove</div>
     </div>
     </div>
 
@@ -67,11 +67,11 @@ function adding (){
     ch22.className='quan';
     ch23.className='max';
     ch21.innerHTML='-';
-    ch21.onclick=()=>{pricedown(str1)};
+    ch21.onclick=()=>{pricedown(str1); foot();};
     ch21.onmouseout=foot;
     ch22.innerHTML=0;
     ch23.innerHTML='+';
-    ch23.onclick=()=>{priceup(str1);};
+    ch23.onclick=()=>{priceup(str1); foot();};
     ch23.onmouseout=foot;
     ch2.append(ch21);
     ch2.append(ch22);
@@ -86,7 +86,6 @@ function adding (){
     ch4.className='ifour';
     ch4.innerHTML='Remove';
     ch4.onclick=()=>{remove(str1);};
-    ch4.onmouseover=foot;
     x.append(ch4);
 }
 
@@ -100,4 +99,5 @@ function remove (value){
         }
         z++;
     }
+    foot();
 }
